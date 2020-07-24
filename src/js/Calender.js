89d9f3abac6b3821c3dc2c6b056295ad1865c7a5
bomee <primavera0331@gmail.com>
2020-07-24 23:44:23 +0900

    JS: to mount calender but having error line 64

--- src/js/Calender.js
+++ src/js/Calender.js
@@ -14,8 +14,9 @@ export default class Calender {
   init() {
     this.bindElements();
     this.bindEvents();
-    this.getFirstDay = (year, month) => this.today(year, month, 1);
-    this.getLastDay = (year, month) => this.today(year, month, 0);
+    this.getFirstDay = (year, month) => new Date(year, month, 1);
+    this.getLastDay = (year, month) => new Date(year, month, 0);
+    this.mountCalender();
   }
   bindElements() {
     this.yearEl = document.querySelector(".calender__active-year");
@@ -49,7 +50,109 @@ export default class Calender {
     this.activeDate = date;
     return date;
   }
+  // SET days and dates
   mountCalender() {
-    // log("mountCalender");
+    this.mountMonthAndYear();
+    this.mountWeek();
+    this.mountDate();
+    // this.bindDateElements();
+  }
+  mountMonthAndYear() {
+    this.yearEl.textContent = this.activeDate.getFullYear();
+    this.monthEl.textContent = this.months[this.activeMonth];
+  }
+  // error the array has all dasy but is mounted just 3 days
+  mountWeek() {
+    debugger;
+    log(this.days);
+    this.thead.innerHTML = this.days.reduce((html, day) => {
+      log(day);
+      log(html);
+      return (html += `<th id="${day}>${day}</th>`);
+    }, "");
+  }
+  mountDate() {
+    const activeDateHtml = this.getActiveDateHtml();
+    this.tbody.innerHTML =
+      activeDateHtml.reduce((html, date) => {
+        return (html += date);
+      }, `<tr>`) + `</tr>`;
+  }
+  getActiveDate() {
+    const year = this.activeDate.getFullYear();
+    const month = this.activeMonth;
+    let days;
+    switch (month) {
+      case 1:
+        return year % 4 === 0 && !year % 100 === 0 ? (days = 29) : (days = 28);
+      case 3:
+      case 5:
+      case 8:
+      case 10:
+        return (days = 30);
+      default:
+        return (days = 31);
+    }
+  }
+  getSpecificDay() {
+    const activeDate = this.activeDate;
+    const activeYear = activeDate.getFullYear();
+    const activeMonth = activeDate.getMonth();
+    const activeFirstDay = this.getFirstDay(activeYear, activeMonth);
+    const prevMonthLastDate = this.getLastDay(activeYear, activeMonth);
+    return {
+      activeFirstDay: activeFirstDay.getDay(),
+      prevMonthLastDate: prevMonthLastDate.getDate(),
+    };
+  }
+  getActiveDateHtml() {
+    const { activeFirstDay, prevMonthLastDate } = this.getSpecificDay();
+    const todayDate = this.today.getDate();
+    const currMonth = this.today.getMonth();
+    const activeDays = this.getActiveDate();
+    const activeYear = this.activeDate.getFullYear();
+    const activeMonth = this.activeMonth;
+    const activeDateHtml = [];
+
+    const currFirstDay = activeFirstDay - 1;
+    let nextMonthDate = 1;
+
+    for (let i = 0; i < 7 * 6; i++) {
+      if (i < activeFirstDay) {
+        activeDateHtml.push(
+          `<td id="${activeYear} -${activeMonth}-${
+            prevMonthLastDate + i - currFirstDay
+          } class="prev">${prevMonthLastDate + i - currFirstDay}</td>`
+        );
+      } else if (i > activeDays + currFirstDay) {
+        activeDateHtml.push(
+          `<td id="${activeYear}-${activeMonth}-${nextMonthDate}" class="next">${nextMonthDate}</td>`
+        );
+        nextMonthDate++;
+      } else {
+        todayDate === i - currFirstDay && currMonth === activeMonth
+          ? activeDateHtml.push(
+              `<td id="${activeYear}-${activeMonth}-${i - currFirstDay}">${
+                i - currFirstDay
+              }</td>`
+            )
+          : activeDateHtml.push(
+              `<td id="${activeYear}-${activeMonth}-${i - currFirstDay}">${
+                i - currFirstDay
+              }</td>`
+            );
+      }
+    }
+    return this.getActiveWeeklyHtml(activeDateHtml);
+  }
+  getActiveWeeklyHtml(dateHtml) {
+    const weeklyHtml = dateHtml;
+    for (let i = 1; i < 6; i++) {
+      const index = i * 7;
+      index === 7
+        ? weeklyHtml.splice(index, 0, `</tr><tr>`)
+        : weeklyHtml.splice(index + i - 1, 0, `</tr><tr>`);
+    }
+    return weeklyHtml;
   }
 }
